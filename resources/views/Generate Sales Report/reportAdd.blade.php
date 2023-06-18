e07d035eadc44e366d9a0c23f708e081c6ba8a4e
Siti Nur Masitah <134575907+NrMasitah@users.noreply.github.com>
2023-06-18 15:17:22 +0800

    Update reportAdd.blade.php

--- resources/views/Generate Sales Report/reportAdd.blade.php
+++ resources/views/Generate Sales Report/reportAdd.blade.php
@@ -1,9 +1,9 @@
+<!DOCTYPE html>
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
 
-@section('content')
     <div class="container">
         <h1>Add Sales Report</h1>
         <form method="POST" action="{{ route('salesreport.store') }}">
